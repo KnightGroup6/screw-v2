1e9a659638f068e0485628c9a7199b58199c552f
richill <knightlearning@knights-Mac-mini.local>
2014-06-02 16:18:32 +0100

    place in validation :name for Job & Trade

--- app/models/job.rb
+++ app/models/job.rb
@@ -3,4 +3,6 @@ class Job < ActiveRecord::Base
 
 	has_many :jobs_trades
   	has_many :trades, through: :jobs_trades
+
+  	validates :name
 end
--- app/models/trade.rb
+++ app/models/trade.rb
@@ -3,4 +3,6 @@ class Trade < ActiveRecord::Base
 
 	has_many :jobs_trades
   	has_many :jobs, through: :jobs_trades
+
+  	validates :name
 end
