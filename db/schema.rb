e76668389078210d99077ae69910ccf5ee956d8e
richill <knightlearning@knights-Mac-mini.local>
2014-06-02 12:25:58 +0100

    created jobs_trades table in schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140602112008) do
+ActiveRecord::Schema.define(version: 20140602112402) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "jobs", force: true do |t|
     t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "jobs_trades", force: true do |t|
+    t.integer  "job_id"
+    t.integer  "trade_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
