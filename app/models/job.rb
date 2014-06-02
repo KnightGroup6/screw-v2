69abaf288fbe43ec20a0810186a6c96cd712cb8d
richill <knightlearning@knights-Mac-mini.local>
2014-06-02 12:29:02 +0100

    created associations between Jobs & Trade models

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,2 +1,5 @@
 class Job < ActiveRecord::Base
+
+	has_many :jobs_trades
+  	has_many :trades, through: :jobs_trades
 end
--- app/models/trade.rb
+++ app/models/trade.rb
@@ -1,2 +1,5 @@
 class Trade < ActiveRecord::Base
+
+	has_many :jobs_trades
+  	has_many :jobs, through: :jobs_trades
 end
