3b9346a78d0545ecca9e1bf108780c6eeaa35391
richill <knightlearning@knights-Mac-mini.local>
2014-06-18 16:05:19 +0100

    in the process of creating a seacrh for job leads for tradesmen

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,2 +1,15 @@
 class Search < ActiveRecord::Base
+
+	# def applications
+	#   @applications ||= find_applications
+	# end
+
+	# def find_applications
+	#   Application.find(:all, :conditions => conditions)
+	# end
+
+	# def keyword_conditions
+	#   ["applications.postcode LIKE ?", "%#{keywords}%"] unless keywords.blank?
+	# end
+
 end
