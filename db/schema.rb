036543798e25edcf24a27bc1591d394f4bc1445f
richill <knightlearning@knights-Mac-mini.local>
2014-06-18 14:01:55 +0100

    created a the search table via g scaffold & migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140617153343) do
+ActiveRecord::Schema.define(version: 20140618130030) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,12 @@ ActiveRecord::Schema.define(version: 20140617153343) do
 
   create_table "jobstartdates", force: true do |t|
     t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "searches", force: true do |t|
+    t.string   "keywords"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
