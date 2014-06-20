2732168c9e717651c61178759f3e7f072fda3f63
richill <knightlearning@knights-Mac-mini.local>
2014-06-20 15:45:31 +0100

    created home location for app

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,8 @@ Rails.application.routes.draw do
 
   resources :jobs
 
+  root to: 'home#index', as: 'home'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
