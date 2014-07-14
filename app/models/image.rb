4d7ec2cd93a5b017e7248aa22f80d79a8cc6d9e2
richill <knightlearning@knights-Mac-mini.local>
2014-07-14 13:06:17 +0100

    successfully added GEM carrierwave

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,4 +1,5 @@
 class Image < ActiveRecord::Base
 	attr_accessible :image, :title, :user_id
 	belongs_to :user
+	mount_uploader :image, ImageUploader
 end
--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -33,7 +33,11 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   # version :thumb do
-  #   process :resize_to_fit => [50, 50]
+  #   process resize_to_fill: [200, 200]
+  # end
+
+  # version :detail do
+  #   process resize_to_fit: [400, 400]
   # end
 
   # Add a white list of extensions which are allowed to be uploaded.
