9cfdfe4248d4036e8bb3d651eae655bfe9586f0c
richill <knightlearning@knights-Mac-mini.local>
2014-07-15 16:15:00 +0100

    reverted routes for users to original route coding

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   resources :images
 
-  devise_for :users, :controllers => { :registrations => :registrations }
+  devise_for :users
   resources :jobstartdates
 
   resources :budgets
