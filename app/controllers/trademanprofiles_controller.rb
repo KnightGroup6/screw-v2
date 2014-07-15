7e47982d387f52a54870905db649b6941d501888
richill <knightlearning@knights-Mac-mini.local>
2014-07-15 13:43:53 +0100

    updated coding for the display of image & trade/jobs in trademans profile

--- app/controllers/trademanprofiles_controller.rb
+++ app/controllers/trademanprofiles_controller.rb
@@ -18,6 +18,7 @@ class TrademanprofilesController < ApplicationController
 
   def editjoblead
     @trades = Trade.order(:name)
+    @jobs = Job.all
 
     if params[:search].present?
       @applications = Application.near(params[:search], 100, order: 'distance')
